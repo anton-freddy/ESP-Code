72132c8373ce6fa62a6d9888128e7e8addefcb12
anton-freddy <118332517+anton-freddy@users.noreply.github.com>
2023-04-05 11:49:36 +1200

    Added Stepper test

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
-#include <stdio.h>
 #include <Arduino.h>
-#include <Servo.h>
+#include <Esp.h>
+#include <SpeedyStepper.h>
 
-#define SERVO_PIN 17
+#define SERVO_PIN 8
+#define CHANNEL 0
+#define PWM_FREQ 50
+#define PWM_RESOLUTION 16
+#define MIN_DUTY 1000
+#define MAX_DUTY 2000
 
-Servo myservo;
+SpeedyStepper S1;
 
 void setup() {
-    pinMode(SERVO_PIN, OUTPUT);
-    Serial.begin(115200);
-    myservo.attach(SERVO_PIN);
-
+  Serial.begin(115200);
+  S1.connectToPins(11,10);
+  pinMode(SERVO_PIN, OUTPUT);
+  S1.setAccelerationInRevolutionsPerSecondPerSecond(20);
+  S1.setSpeedInRevolutionsPerSecond(20);
 }
 
-int pos = 0;
+// void loop() {
+//   digitalWrite(SERVO_PIN, HIGH);
+//   delay(10);
+//   digitalWrite(SERVO_PIN, LOW);
+//   delay(10);
+// }
 
-void loop() {
-  analogWrite(SERVO_PIN, 50);
+
+void loop(){
+
+  S1.moveRelativeInRevolutions(600);
+
+
 }
