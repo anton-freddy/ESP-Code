1c67e9dd810592fdf235d0f76d29281ff0ce997d
anton-freddy <118332517+anton-freddy@users.noreply.github.com>
2023-04-06 11:24:20 +1200

    Init Commit

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,36 @@
 #define MIN_DUTY 1000
 #define MAX_DUTY 2000
 
-SpeedyStepper S1;
+SpeedyStepper S1, S2;
 
 void setup() {
   Serial.begin(115200);
-  S1.connectToPins(11,10);
-  pinMode(SERVO_PIN, OUTPUT);
-  S1.setAccelerationInRevolutionsPerSecondPerSecond(20);
-  S1.setSpeedInRevolutionsPerSecond(20);
-}
+  S1.connectToPins(13,14);
+  S2.connectToPins(11,12);
+  //S2.setStepsPerRevolution(400);
+  S2.setStepsPerMillimeter(20.54647908);
+  //S1.setStepsPerRevolution(400);
+  S1.setStepsPerMillimeter(20.54647908);
+  
+  S1.setAccelerationInMillimetersPerSecondPerSecond(300);
+  S1.setSpeedInMillimetersPerSecond(1500);
 
-// void loop() {
-//   digitalWrite(SERVO_PIN, HIGH);
-//   delay(10);
-//   digitalWrite(SERVO_PIN, LOW);
-//   delay(10);
-// }
+  
+  S2.setAccelerationInMillimetersPerSecondPerSecond(300);
+  S2.setSpeedInMillimetersPerSecond(1500);
+
+  S1.setupMoveInMillimeters(10000);
+  S2.setupMoveInMillimeters(10000);
+}
 
 
 void loop(){
 
-  S1.moveRelativeInRevolutions(600);
-
+  if(S1.processMovement()){
+    S1.setupMoveInMillimeters(10000);
+  }
+  if(S2.processMovement()){
+    S2.setupMoveInMillimeters(10000);
+  }
 
 }
